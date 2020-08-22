c677cad37c2ff52e7d542902452fede47e5f1ef1
Lucas Minter <lsminteris@gmail.com>
2020-08-21 21:05:50 -0700

    removed unneccessary code in fetch requets

--- src/App.js
+++ src/App.js
@@ -1,32 +1,20 @@
 import React from 'react';
 import './App.css';
 
-require('dotenv').config()
-
-
 const twitchURL = 'https://api.twitch.tv/helix/streams?user_login=gamesdonequick'
-const BEARER_TOKEN = process.env.BEARER_TOKEN
-const CLIENT_ID = process.env.CLIENT_ID
+const BEARER_TOKEN = 
+const CLIENT_ID = 
 
 
 
 function App() {
   function Streamer() {  
     async function getData(url = '', data = {}) {
-      // Default options are marked with *
       const response = await fetch(url, {
-        method: 'GET', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, *same-origin, omit
-        headers: new Headers({
-          'Content-Type': 'application/json',
-          // 'Content-Type': 'application/x-www-form-urlencoded',
+        headers: ({
           'Authorization': `Bearer ` + BEARER_TOKEN,
           'Client-ID': CLIENT_ID
         }),
-        redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
       });
       return response.json(); // parses JSON response into native JavaScript objects
     }
